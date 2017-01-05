bc093de993806230f3dd5df7bbcd0e5cd5ae504b
Sam Samai <sam@studio2pi.com.au>
2017-01-05 18:58:52 +1100

    Adds paperclip gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_attached_file :avatar, styles: { medium: "300x300", thumb: "100x100" }
+  validates_attachment :avatar,
+     content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
   
   root to: 'landing_page#show'
   
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+        sessions: 'users/sessions',
+        passwords: 'users/passwords',
+        registrations: 'users/registrations'
+  }
 end
