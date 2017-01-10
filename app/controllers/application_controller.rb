17ee6038f95ab12359b65e69b81e730c56a71ebf
Jeffrey Bartels <jeffreybartels@gmail.com>
2017-01-10 18:52:36 +1100

    Adding parameters to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,9 @@ class ApplicationController < ActionController::Base
   protected
  
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :password_confirmation, :remember_me, :avatar, :avatar_cache])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password, :password_confirmation, :remember_me, :avatar, :avatar_cache])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :password_confirmation, :remember_me, :avatar, :avatar_cache, :first_name, :last_name, :phone])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password, :password_confirmation, :remember_me, :avatar, :avatar_cache, :first_name, :last_name, :phone])
   end
 end
+
+
